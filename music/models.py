f5f64a1baf781fb71c4076644eb4c78ce91e4323
ravi <raviteja.kollapur@gmail.com>
2018-04-10 16:10:18 +0530

    New files added

---
+++ music/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# This denotes a table Album with columns artist,album_title etc
+
+class Album(models.Model):
+
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=250)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return "{}-{}".format(self.album_title,self.artist)
+
+
+class Song(models.Model):
+
+    album = models.ForeignKey(Album,on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
+
+
+# After any changes in models file. 2 steps will replicate changes
+# 1. Make migrate app - python manage.py makemigrations music
+# 2. Actual migrate - python manage.py migrate
+
+
+
