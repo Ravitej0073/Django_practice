845fc2ee9acb2cf869d611912908144c5de229da
ravi <raviteja.kollapur@gmail.com>
2018-04-17 16:33:42 +0530

    Added new LOC

--- music/models.py
+++ music/models.py
@@ -18,6 +18,10 @@ class Song(models.Model):
     album = models.ForeignKey(Album,on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+    is_favourite=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title
 
 
 # After any changes in models file. 2 steps will replicate changes
--- music/views.py
+++ music/views.py
@@ -1,6 +1,27 @@
-from django.http import HttpResponse
+from .models import Album
+from django.shortcuts import render,get_object_or_404
+
 
 def index(request):
-    return HttpResponse("<h1>This is music homepage</h1>")
+    all_albums = Album.objects.all()
+    # html = ''
+    # for album in all_albums:
+    #     url = "/music/{}".format(album.id)
+    #     html += '<a href={}> {} </a> <br>'.format(url,album.album_title)
+    # template = loader.get_template('music/index.html')
+
+    # context = {'all_albums':all_albums}
+    # return HttpResponse(template.render(context,request))
+    # return render(request,'music/index.html',context)
+    return render(request,'music/index.html',{'all_albums':all_albums})
+
+
+def details(request,album_id):
+    # return HttpResponse("<h2>Details for Album id {}</h2>".format(album_id))
+
+    album=get_object_or_404(Album,pk=album_id)
+    return render(request,'music/details.html',{'album':album})
+
+
 
 
